2bf67e3756c28d00229468597a6697c65e696f70
Ali  Haider <justalihaider@gmail.com>
2021-09-02 06:25:07 +0300

    stored class in varriable

--- script.js
+++ script.js
@@ -1,31 +1,6 @@
 'use strict';
 
 const modal = document.querySelector('.modal');
-const showModal = document.querySelectorAll('.show-modal');
-const closeModal = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
-
-let openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-}
-let removeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-}
-let closeOnOverlay = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-}
-
-for (let i = 0; i < showModal.length; i++)
-  showModal[i].addEventListener('click', openModal)
-closeModal.addEventListener('click', removeModal);
-overlay.addEventListener('click', closeOnOverlay);
-
-document.addEventListener('keydown', function (e) {
-  // console.log(e.key);sdf
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeOnOverlay();
-  }
-});
+const CloseModalBtn = document.querySelector('.close-modal');
+cosnt showModals = document.querySelectorAll('.open-modal');
