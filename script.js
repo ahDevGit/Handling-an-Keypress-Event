0b88cd37d3858a3c41b7ea199ded1ca3422faab0
Ali  Haider <justalihaider@gmail.com>
2021-09-02 06:49:23 +0300

    Close Modal Function Update

--- script.js
+++ script.js
@@ -3,4 +3,20 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const CloseModalBtn = document.querySelector('.close-modal');
-cosnt showModals = document.querySelectorAll('.open-modal');
+const showModalsBtn = document.querySelectorAll('.show-modal');
+
+console.log(showModalsBtn);
+
+for (let i = 0; i < showModalsBtn.length; i++)
+  showModalsBtn[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+CloseModalBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
