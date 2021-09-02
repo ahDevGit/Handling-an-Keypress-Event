9d7d0d3ff362d43f8fea61c1aa60932b610c37ac
Ali  Haider <justalihaider@gmail.com>
2021-09-02 06:52:47 +0300

    Function Created for Open and Close Modal

--- script.js
+++ script.js
@@ -5,18 +5,21 @@ const overlay = document.querySelector('.overlay');
 const CloseModalBtn = document.querySelector('.close-modal');
 const showModalsBtn = document.querySelectorAll('.show-modal');
 
-console.log(showModalsBtn);
-
-for (let i = 0; i < showModalsBtn.length; i++)
-  showModalsBtn[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-
+//Open Modal Function
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+//Close Modal Function 
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 }
+
+for (let i = 0; i < showModalsBtn.length; i++)
+  showModalsBtn[i].addEventListener('click', showModal);
+
+
 
 CloseModalBtn.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
